ef9651e689431e6c72c8a2446a18cf7f2c50ff62
brunohendias <brunohen.dias@gmail.com>
2019-09-21 17:08:29 -0300

    organizacao

--- minijogos/Bingo.py
+++ minijogos/Bingo.py
@@ -5,9 +5,6 @@ Feito por Bruno Henrique
       09/10/2018
 Caso fassa alguma alteração poste no github e mande o link
 github: https://github.com/brunohendias
-
-Alterado dia 19/04/2019
-Alterado dia 26/04/2019
 """
 from random import randint
 import time
@@ -17,16 +14,6 @@ print("~="*38)
 print(f" Seja bem vindo {nome} ao Bingo! Sera hoje o seu dia de sorte?!")
 print(" Aperte Enter para sortear\n digite sair para sair\n digite cartela para ver a sua cartela\n Digite sorteados para ver os numeros sorteados")
 print("~="*38)
-
-def cria_cartela():
-    cartela = []
-    tamanho_cartela = 20
-    for total in range(tamanho_cartela):
-        numero = randint(0, 80)
-        while numero in cartela:
-            numero = randint(0, 80)
-        cartela.append(numero)
-    return cartela
 
 def start_game():
     intervalo, total_sorteios, acerto = 3, 60, 0
@@ -39,6 +26,16 @@ def start_game():
         nova_cartela = input("\n Para trocar a cartela digite trocar ou aperte Enter para começar: ").lower()
     print("\n Numeros sorteados abaixo\n")
     sorteio(cartela, intervalo, acerto, total_sorteios)
+
+def cria_cartela():
+    cartela = []
+    tamanho_cartela = 20
+    for total in range(tamanho_cartela):
+        numero = randint(0, 80)
+        while numero in cartela:
+            numero = randint(0, 80)
+        cartela.append(numero)
+    return cartela
 
 def sorteio(cartela, intervalo, acerto, total_sorteados):
     numeros_sorteados = []
@@ -60,6 +57,4 @@ def sorteio(cartela, intervalo, acerto, total_sorteados):
             print(f" {num}", end="", flush=True)
         time.sleep(intervalo)
 
-def main():
-    start_game()
-main()
+start_game()
