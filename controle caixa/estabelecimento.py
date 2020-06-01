a3870885cd9aa3eb97a0a6fe2dbc0d10d09f1f8a
brunohendias <brunohdiasss@gmail.com>
2020-06-01 19:55:37 -0300

    opcao ver fila caixa, estrutura

--- controle caixa/estabelecimento.py
+++ controle caixa/estabelecimento.py
@@ -1,25 +1,61 @@
+#!/usr/bin/python3
 from caixa import Caixa
 import os
 
-print("=~"*32)
+os.system('cls')
+print("=~="*25)
 print("""
-    Bem vindo ao controle de caixas
-    Menu:
-        [1] Adicionar funcionario para o caixa
-        [2] Adicionar cliente
-        [3] Remover cliente
+    Programa para automatizar o controle de fila
+    Github: https://github.com/brunohendias
+    Data: 09/05/2020
 """)
-print("=~"*32)
-comeca = input("Aperte ENTER para iniciar... ")
-opcao = input("\nVamos começar, digite a opção escolhida: ")
+print("=~="*25)
+input("Aperte ENTER para iniciar... ")
+print("\nPrimeiro vamos adicionar os caixas\nDigite (fim) para o proximo passo\n")
 
-supermercado = Caixa()
-if opcao == '1':
-    supermercado.adiciona_caixa()
-elif opcao == '2':
-    supermercado.entrar()
-elif opcao == '3':
-    supermercado.sair()
+estabelecimento = Caixa()
+continua = True
+while continua:
+    caixa = input("Nome do caixa: ")
+    if caixa == 'fim':
+        continua = False
+        break
+    caixa = estabelecimento.adiciona_caixa(caixa)
+    print(f"Caixas: {' '.join(estabelecimento.nom_caixas)}\n")
 
-print(supermercado.caixas)
+print("\nAgora vamos começar\n")
+print("=~="*25)
+print(f"""
+            Menu de opções
+            
+    [0] Finalizar o programa    
+    [1] Adicionar caixa
+    [2] Adicionar cliente
+    [3] Remover cliente
+    [4] Ver a fila do caixa
+
+    Caixas: {' '.join(estabelecimento.nom_caixas)}
+""")
+print("=~="*25)
+
+opcao = input("Digite a opção: ")
+while opcao != '0':
+    if opcao == '1':
+        estabelecimento.adiciona_caixa()
+        
+    elif opcao == '2':
+        nome = input("Nome do cliente: ")
+        estabelecimento.entrar(nome)
+        
+    elif opcao == '3':
+        estabelecimento.sair()
+
+    elif opcao == '4':
+        nom_caixa = input("Digite o nome do caixa: ")
+        print(f"\n Caixa: {nom_caixa}\n Fila: {' '.join(estabelecimento.ver_fila_caixa(nom_caixa))}")
+
+    else:
+        print("Opção invalida")
+
+    opcao = input("\nDigite a opção: ")
     
