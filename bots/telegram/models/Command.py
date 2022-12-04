31a3f6f92c11d66ec53dca4b8a521f6542335fc9
bruno dias <brunodias@bruno.local>
2022-12-03 22:30:20 -0300

    removendo models antiga

--- bots/telegram/models/Command.py
+++
@@ -1,7 +0,0 @@
-class Command():
-    def __init__(self, command, action):
-        self.command = command
-        self.action = action
-        
-    def __repr__(self):
-        return f"{self.command}"
