852fa9416adf5c5afd9155b9b75e6ff2aa2d613b
bruno dias <brunodias@bruno.local>
2022-12-04 15:08:39 -0300

    random password length

--- bots/telegram/whiskyh/utils.py
+++ bots/telegram/whiskyh/utils.py
@@ -5,7 +5,7 @@ def SOCommand(command: str):
     return popen(command).read()
 
 def randomPassword():
-    return SOCommand('openssl rand -base64 30')
+    return SOCommand('openssl rand -base64 50')
 
 commands = [
     Command('password', randomPassword)
