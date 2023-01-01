f82bd92c8abf263605e07571e298b38f255b67f5
bruno dias <brunodias@bruno.local>
2023-01-01 20:24:40 -0300

    change branch

--- controle_caixa/Classes/Caixas/caixa.py
+++ controle_caixa/Classes/Caixas/caixa.py
@@ -1,16 +1,18 @@
-#!/usr/bin/python3
 class Caixa:
-
+    nome = ''
+    dinheiro = 1000.00
+    fila = []
+    limite = 5
     def __init__(self):
-        self.nome = ''
-        self.dinheiro_inicial = 1000.00
-        self.dinheiro_final = 1000.00
-        self.fila = []
-        self.nom_clientes = []
-        self.limiteFila = 5
-
+        self.dinheiro_inicial = self.dinheiro
     def __str__(self):
-        return f"\nNome: {self.nome}\nDinheiro inicial: R${self.dinheiro_inicial}\nDinheiro atual: R${round(self.dinheiro_final, 2)}\nFila: {self.fila}\nLimite: {self.limiteFila}"
-
+        return f"\nNome: {self.nome}\nDinheiro: R${round(self.dinheiro, 2)}\nFila: {self.fila}\nLimite: {self.limiteFila}"
     def __repr__(self):
     	return self.nome
+    def add_cliente(self, cliente):
+        if len(self.fila) < self.limite:
+            self.fila.append(cliente)
+    def rm_cliente(self, cliente):
+        if cliente < 0 or cliente > len(self.fila):
+            return 'cliente nao esta na fila'
+        del(self.fila[cliente])
--- controle_caixa/Uteis/listas.py
+++ controle_caixa/Uteis/listas.py
@@ -1,10 +1,4 @@
-partileiras = ['Arroz','Atum','Azeite','Azeitona','Batata_palha','Baunilha','Biscoito','Bombom','Cafe','Caldo','Catchup','Cereal','Cha','Champignon','Chocolate','Chocolate granulado','Coco ralado','Creme de leite','Farinha de mandioca','Farinha de milho','Farinha de rosca','Farinha de trigo','Fermento','Gelatina','Geleia','Leite','Leite condensado','Leite de coco','Lentilha','Macarrãoo','Maionese','Molho','Mostarda','Polpa de tomate','Polvilho','Sagu','Sal','Sal grosso','Salsicha','Sardinha','Sopa','Tempero']
-
-preco = [96, 89, 8, 81, 89, 28, 94, 25, 74, 72, 34, 29, 86, 14, 65, 41, 78, 96, 47, 11, 25, 78, 54, 59, 80, 16, 89, 25, 14, 100, 97, 1, 80, 1, 69, 4, 88, 39, 11, 22, 91, 93]
-
-preco_decimal = [87.46, 63.47, 13.85, 15.83, 44.29, 22.79, 36.71, 12.86, 97.15, 8.19, 8.54, 3.77, 49.79, 70.51, 74.52, 32.62, 62.12, 14.94, 43.51, 10.68, 50.91, 85.68, 84.35, 90.58, 20.48, 69.71, 72.69, 90.18, 75.46, 21.29, 3.14, 19.41, 17.79, 83.0, 52.43, 86.48, 84.68, 92.98, 18.31, 54.12, 97.49, 66.71]
-
-produto_preco = [
+produtos = [
 	['Fermento',24.68],['Gelatina',60.47],['Geleia',84.61],['Leite',75.46],['Leite condensado',42.38],['Leite de coco',95.42],
 	['Polvilho',48.05],['Sagu',73.7],['Sal',69.47],['Sal grosso',52.47],['Salsicha',32.45],['Sardinha',26.61],['Sopa',78.49],
 	['Farinha de mandioca',22.66],['Farinha de milho',69.58],['Farinha de rosca',43.81],['Farinha de trigo',24.33],['Atum',83.0],
@@ -13,9 +7,3 @@ produto_preco = [
 	['Lentilha',57.6],['Macarrãoo',33.6], ['Maionese',60.28],['Molho',50.31],['Mostarda',6.94],['Polpa de tomate',59.93],
 	['Arroz',96.75],['Azeite',88.41],['Azeitona',45.39],['Batata_palha',59.65],['Baunilha',43.19],['Tempero',24.58]
 ]
-
-# from random import uniform
-# lista = []
-# for i in range(len(partileiras)):
-# 	numero = round(uniform(5, 100), 2)
-# 	lista.append([partileiras[i], numero])
